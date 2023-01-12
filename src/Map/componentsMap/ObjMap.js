56aa9def9cc9a950f456b882f460c7847bc6633b
loicqql <loic.qql@gmail.com>
2023-01-12 09:53:56 +0100

    fix path gltf

--- src/Map/componentsMap/ObjMap.js
+++ src/Map/componentsMap/ObjMap.js
@@ -6,7 +6,9 @@ import { useGLTF } from '@react-three/drei'
 
 // import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
-useGLTF.preload('/models/map_meshed_modifier.gltf')
+const file = "map_meshed_modifier.gltf";
+
+useGLTF.preload('/models/' + file)
 
 // based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a) 
 // by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
@@ -15,7 +17,7 @@ export function ObjMap() {
   //   GLTFLoader,
   //   process.env.PUBLIC_URL + "models/map_meshed_test1.glb"
   // );
-  const t = useGLTF('/models/map_meshed_modifier.gltf')
+  const t = useGLTF('/models/' + file)
 
   // const obj = useLoader(OBJLoader, '/models/')
   // const fbx = useLoader(FBXLoader, '/models/map_uncolored_fbx.fbx')
