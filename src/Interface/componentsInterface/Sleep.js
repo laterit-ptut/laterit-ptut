2c501a24930b112ae44a207b7066b8097afc320b
loicqql <loic.qql@gmail.com>
2023-01-25 15:49:15 +0100

    line on sleep screen

--- src/Interface/componentsInterface/Sleep.js
+++ src/Interface/componentsInterface/Sleep.js
@@ -15,8 +15,10 @@ export function Sleep({setSleep, setActivePoint}) {
 
   return (
     <div className={`Sleep ${opacity ? '' : 'wakeup'}`} style={{backgroundImage: `url('/medias/images/PontdeFiherenana.jpg')`}} onClick={handleClick}>
+      <img className="line line1" src="/icons/line.svg" alt="" />
+      <img className="line line2" src="/icons/line.svg" alt="" />
       <div className="Sleep_button">
-        <img src="/icons/arrow.svg" />
+        <img src="/icons/arrow.svg" alt="" />
       </div>
     </div>
   );
